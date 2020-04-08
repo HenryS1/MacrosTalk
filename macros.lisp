48de773c2bafa8a48a1da0c9bec405b46aaf2da4
Henry Steere <henry.steere@gmail.com>
2020-04-08 21:55:39 +0200

    Add neat lambda syntax

--- macros.lisp
+++ macros.lisp
@@ -15,3 +15,24 @@
 
 (defmacro -?> (&rest exps)
   (reduce #'bind-subst exps))
+
+(defun find-args (exp)
+  (labels ((rec (e acc)
+               (cond ((and (symbolp e) (char= (aref (symbol-name e) 0) #\%))
+                      (adjoin e acc)) 
+                     ((consp e)
+                      (let ((args (rec (car e) acc)))
+                        (rec (cdr e) args)))
+                     (t acc))))
+      (rec exp nil)))
+
+(defmacro expand-lambda (exp)
+  `(let ((args (find-args ,exp)))
+    `(lambda ,args ,exp)))
+
+(defun |#f-reader| (stream subchar arg)
+  (declare (ignore subchar arg))
+  (let ((exp (read stream)))
+    (expand-lambda exp)))
+
+(set-dispatch-macro-character #\# #\f #'|#f-reader|)
